c16ad29a5685d812160e1af9b60c840f4e1062fe
Abdul Basit Ansari <114910040+Abdul-Basit-Pro@users.noreply.github.com>
2023-01-31 16:04:21 +0500

    Initial Commit

---
+++ output.tf
@@ -0,0 +1,34 @@
+output "nat_public_ips" {
+  description = "List of public Elastic IPs created for AWS NAT Gateway"
+  value       = aws_eip.nateIP.public_ip
+}
+
+output "aws_db_subnet_group_name" {
+  description = "subnet group for  rds"
+  value       = aws_db_subnet_group.rds.name
+}
+
+output "pub_subnet_for_ec2" {
+  description = "subnet for ec2"
+  value       = aws_subnet.publicsubnets[0].id
+}
+
+output "vpc_cidr" {
+  description = "vpc cidr"
+  value       = var.main_vpc_cidr
+}
+
+output "vpc_id" {
+  description = "vpc id"
+  value       = aws_vpc.Main.id
+}
+
+output "web_sec_id" {
+  description = "sec id for ec2"
+  value       = aws_security_group.web.id
+}
+
+output "mysql_sec_id" {
+  description = "sec id for mysql"
+  value       = aws_security_group.mysql.id
+}
